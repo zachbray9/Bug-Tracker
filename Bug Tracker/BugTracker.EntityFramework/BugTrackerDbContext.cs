74c3f3569b29c648e1aa54f076e96b397fbeea2b
Zachary Bray <zachbray9@knights.ucf.edu>
2023-01-05 20:02:18 -0500

    Created new class libraries for Domain and Entity Framework. Began setting up database.

---
+++ Bug Tracker/BugTracker.EntityFramework/BugTrackerDbContext.cs
@@ -0,0 +1,22 @@
+﻿using BugTracker.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.EntityFramework
+{
+    public class BugTrackerDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=BugTrackerDB;Trusted_Connection=True");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}

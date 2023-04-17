b4e98b680e35dc58db79921b46c9dfe58f1fe859
Zachary Bray <zachbray9@knights.ucf.edu>
2023-04-17 01:58:13 -0400

    Added linq query for Tickets page
    Continued making UI for tickets view and added linq query to TicketsPageViewModel to load Ticket data from database.

--- Bug Tracker/Bug Tracker/ViewModels/TicketsPageViewModel.cs
+++ Bug Tracker/Bug Tracker/ViewModels/TicketsPageViewModel.cs
@@ -3,6 +3,7 @@ using BugTracker.Domain.Models;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,25 @@ namespace Bug_Tracker.ViewModels
     public class TicketsPageViewModel : ViewModelBase
     {
         private readonly IAuthenticator Authenticator;
+        public ObservableCollection<Ticket> Tickets { get; set; }
 
         public TicketsPageViewModel(IAuthenticator authenticator)
         {
             Authenticator = authenticator;
+            Tickets = new ObservableCollection<Ticket>();
+
+            ResetTickets();
+        }
+
+        private void ResetTickets()
+        {
+            if (Authenticator.CurrentUser.ProjectUsers != null)
+            {
+                var tickets = from projectUser in Authenticator.CurrentUser.ProjectUsers
+                              from Ticket in projectUser.Tickets
+                              orderby Ticket?.DateSubmitted descending
+                              select Ticket;
+            }
         }
 
   

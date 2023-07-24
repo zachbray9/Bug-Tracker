4ac8f9345f7a2a4d70b5dc8351404ee0740ec9d6
Zachary Bray <zachbray9@knights.ucf.edu>
2023-07-23 23:46:47 -0400

    Created a Debounce Timer for TicketDetailsViewModel
    Created a Debounce Timer for the TicketDetailsViewModel. This makes it so that if the user types in the Title or Description box, the changes aren't saved until after 500 milliseconds of not typing. This prevents a ton of Database requests at once.

--- Bug Tracker/Bug Tracker/ViewModels/TicketDetailsPageViewModel.cs
+++ Bug Tracker/Bug Tracker/ViewModels/TicketDetailsPageViewModel.cs
@@ -4,26 +4,40 @@ using BugTracker.Domain.Models;
 using BugTracker.Domain.Services;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 
 namespace Bug_Tracker.ViewModels
 {
-    public class TicketDetailsPageViewModel : ViewModelBase
+    public class TicketDetailsPageViewModel : ViewModelBase, IDisposable
     {
         public IProjectContainer ProjectContainer { get; }
         private readonly IDataService<Ticket> TicketDataService;
 
         public Ticket CurrentTicket => ProjectContainer.CurrentTicket;
 
+        //DebounceTimer is so that changes are only saved to the database after a few seconds of inactivity so the thread isn't overloaded with db requests.
+        private DispatcherTimer DebounceTimer { get; }
+
         public TicketDetailsPageViewModel(IProjectContainer projectContainer, IDataService<Ticket> ticketDataService)
         {
             ProjectContainer = projectContainer;
             TicketDataService = ticketDataService;
 
+            DebounceTimer = new DispatcherTimer()
+            {
+                Interval = TimeSpan.FromMilliseconds(500),
+                IsEnabled = false  
+            };
+
+            DebounceTimer.Tick += DebounceTimer_Tick;
+
             TicketTitle = CurrentTicket.Title;
             TicketDescription = CurrentTicket.Description;
+
         }
 
         private string ticketTitle;
@@ -34,11 +48,7 @@ namespace Bug_Tracker.ViewModels
             {
                 ticketTitle = value;
                 OnPropertyChanged(nameof(TicketTitle));
-
-                if(TicketTitle != CurrentTicket.Title)
-                {
-                    SaveChanges();
-                }
+                StartDebounceTimer();   
             }
         }
 
@@ -50,22 +60,39 @@ namespace Bug_Tracker.ViewModels
             {
                 ticketDescription = value;
                 OnPropertyChanged(nameof(TicketDescription));
-
-                if(TicketDescription != CurrentTicket.Description)
-                {
-                    SaveChanges();
-                }
+                StartDebounceTimer();
             }
         }
 
 
-        private async void SaveChanges()
+        private async Task SaveChangesAsync()
         {
             CurrentTicket.Title = TicketTitle;
             CurrentTicket.Description = TicketDescription;
 
             await TicketDataService.Update(CurrentTicket.Id, CurrentTicket);
+            System.Diagnostics.Debug.WriteLine("Changes have been saved");
         }
 
+        private void StartDebounceTimer()
+        {
+            if(DebounceTimer.IsEnabled)
+            {
+                DebounceTimer.Stop();
+            }
+
+            DebounceTimer.Start();
+        }
+
+        private async void DebounceTimer_Tick(object sender, EventArgs e)
+        {
+            DebounceTimer.Stop();
+            await SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            DebounceTimer.Tick -= DebounceTimer_Tick;
+        }
     }
 }

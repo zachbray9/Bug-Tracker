fa7527ed10da5fe25b003fa8d139f63083b96cc9
Zachary Bray <zachbray9@knights.ucf.edu>
2024-06-14 22:57:10 -0400

    Added resize buttons to cropper slider
    Added min zoom and max zoom buttons on the profile picture cropper slider.

--- Bug Tracker/agileproreactapp/src/components/common/ImageUpload/PhotoCropper.tsx
+++ Bug Tracker/agileproreactapp/src/components/common/ImageUpload/PhotoCropper.tsx
@@ -1,6 +1,7 @@
-import { Box, Slider, SliderFilledTrack, SliderThumb, SliderTrack } from "@chakra-ui/react"
+import { Flex, IconButton, Slider, SliderFilledTrack, SliderThumb, SliderTrack, Stack } from "@chakra-ui/react"
 import { RefObject, useState } from "react"
 import AvatarEditor from "react-avatar-editor"
+import { FaRegImage } from "react-icons/fa6"
 
 interface Props {
     image: any
@@ -9,17 +10,24 @@ interface Props {
 
 export default function PhotoCropper({ image, editorRef }: Props) {
     const [scale, setScale] = useState(1);
+    const sliderFilledTrackBg = "#2c3e5d";
 
     return (
-        <Box>
+        <Stack gap={4}>
             <AvatarEditor image={image} ref={editorRef} width={200} height={200} scale={scale} borderRadius={100} />
 
-            <Slider value={scale} onChange={(value) => setScale(value)} min={1} max={3}>
-                <SliderTrack>
-                    <SliderFilledTrack />
-                </SliderTrack>
-                <SliderThumb />
-            </Slider>
-        </Box>
+            <Flex gap={4}>
+                <IconButton aria-label="Zoom out" icon={<FaRegImage />} size="xs" fontSize="10px" onClick={() => setScale(1)} />
+
+                <Slider value={scale} onChange={(value) => setScale(value)} min={1} max={3} step={0.01}>
+                    <SliderTrack>
+                        <SliderFilledTrack bg={sliderFilledTrackBg} />
+                    </SliderTrack>
+                    <SliderThumb bg={sliderFilledTrackBg} />
+                </Slider>
+
+                <IconButton aria-label="Zoom in" icon={<FaRegImage />} size="xs" fontSize="18px" onClick={() => setScale(3)} />
+            </Flex>
+        </Stack>
     )
 }

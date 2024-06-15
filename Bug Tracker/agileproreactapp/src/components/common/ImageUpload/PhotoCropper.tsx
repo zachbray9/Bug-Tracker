6b6fc7a2482e6b68ac73f643ec999bb9df68679e
Zachary Bray <zachbray9@knights.ucf.edu>
2024-06-14 21:26:13 -0400

    Added slider to image cropper
    Added a slider to the profile picture cropping component to adjust image zoom.

--- Bug Tracker/agileproreactapp/src/components/common/ImageUpload/PhotoCropper.tsx
+++ Bug Tracker/agileproreactapp/src/components/common/ImageUpload/PhotoCropper.tsx
@@ -1,4 +1,5 @@
-import { RefObject } from "react"
+import { Box, Slider, SliderFilledTrack, SliderThumb, SliderTrack } from "@chakra-ui/react"
+import { RefObject, useState } from "react"
 import AvatarEditor from "react-avatar-editor"
 
 interface Props {
@@ -7,7 +8,18 @@ interface Props {
 }
 
 export default function PhotoCropper({ image, editorRef }: Props) {
+    const [scale, setScale] = useState(1);
+
     return (
-        <AvatarEditor image={image} ref={editorRef} width={200} height={200} scale={1.2} borderRadius={100} />
+        <Box>
+            <AvatarEditor image={image} ref={editorRef} width={200} height={200} scale={scale} borderRadius={100} />
+
+            <Slider value={scale} onChange={(value) => setScale(value)} min={1} max={3}>
+                <SliderTrack>
+                    <SliderFilledTrack />
+                </SliderTrack>
+                <SliderThumb />
+            </Slider>
+        </Box>
     )
 }

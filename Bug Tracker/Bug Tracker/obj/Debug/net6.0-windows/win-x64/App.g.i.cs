df52c3d732de1f4d6319cb2f9b8298c1a53b0242
Zachary Bray <zachbray9@knights.ucf.edu>
2024-07-31 23:45:39 -0400

    Made Settings Pages Responsive
    Added responsive styles to the settings pages including the Account Settings, Preferences pages.

--- Bug Tracker/Bug Tracker/obj/Debug/net6.0-windows/win-x64/App.g.i.cs
+++ Bug Tracker/Bug Tracker/obj/Debug/net6.0-windows/win-x64/App.g.i.cs
@@ -53,7 +53,7 @@ namespace Bug_Tracker {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Bug Tracker;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Bug Tracker;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);

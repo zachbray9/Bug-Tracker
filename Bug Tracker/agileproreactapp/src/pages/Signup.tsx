d3b260f584a554ebfd1f1de343c2a8dc032eaab3
Zachary Bray <zachbray9@knights.ucf.edu>
2024-08-07 02:21:20 -0400

    Added feature for filtering tickets based on search query
    Added a search bar on the project board page that allows users to filter tickets based on a search query. Also moved static images from the project to azure blob storage. Also fixed issue where ticket column color was too light in light mode.

--- Bug Tracker/agileproreactapp/src/pages/Signup.tsx
+++ Bug Tracker/agileproreactapp/src/pages/Signup.tsx
@@ -1,7 +1,5 @@
 import { Card, CardHeader, CardBody, CardFooter, Heading, Image, Text, Center, } from "@chakra-ui/react";
 import { NavLink } from "react-router-dom";
-import Image1 from "../assets/AgileProLoginPageImage1.png";
-import Image2 from "../assets/AgileProLoginPageImage2.png";
 import RegisterForm from "../components/RegisterForm";
 import { Helmet } from "react-helmet-async";
 
@@ -14,8 +12,8 @@ export default function Signup(){
             </Helmet>
 
             <Center pos="relative" minH="calc(100svh - 60px)" paddingX="1rem">
-                <Image src={Image1} pos='absolute' bottom='0' left={{base: '1rem', md: '3rem'}} boxSize={[0, 200, 300, 400]} />
-                <Image src={Image2} pos='absolute' bottom='0' right={{ base: '1rem', md: '3rem' }} boxSize={[0, 200, 300, 400]} />
+                <Image src='https://agileproblobstorage.blob.core.windows.net/site-assets/AgileProLoginPageImageRocket.png' pos='absolute' bottom='0' left={{base: '1rem', md: '3rem'}} boxSize={[0, 200, 300, 400]} />
+                <Image src='https://agileproblobstorage.blob.core.windows.net/site-assets/AgileProLoginPageImageTimer.png' pos='absolute' bottom='0' right={{ base: '1rem', md: '3rem' }} boxSize={[0, 200, 300, 400]} />
 
                 <Card variant='outline' align='center' maxW='lg' width='100%' boxShadow='rgba(0, 0, 0, 0.24) 0px 3px 8px'>
                     <CardHeader>
